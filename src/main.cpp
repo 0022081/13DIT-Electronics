b5a7c80ab8f661bd4c8377f99ce6a7327580e7f3
0022081 <benjamin10beard@gmail.com>
2025-06-13 10:14:46 +1200

    Add RTC_DS3231 (Date & Time)

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,70 @@
 
-#include "Arduino.h"
+// Including Libraries 
+#include <Arduino.h>
 #include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 #include <DHT.h>
 #include <DHT_U.h>
 
+#include <RTClib.h>
+
+// Defining PINS
 #define DHTPIN 12
 
+
+// Defining Device Types
 #define DHTTYPE    DHT11 
 
-DHT_Unified dht(DHTPIN, DHTTYPE);
+//Define Objects
+DHT_Unified dht(DHTPIN, DHTTYPE); //Create dht object
+RTC_DS3231 rtc; //Create rtc object
 
+// Set delay time structure
 uint32_t delayMS;
+
+char t[32];
 
 void setup()
 {
+    // Initialize devices
   Serial.begin(9600);
-  // Initialize device.
+  Wire.begin();
+  rtc.begin();
   dht.begin();
 
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Set initial time
+
+    while (!Serial); // Wait for serial port to be ready
+  if (! rtc.begin()) {
+    Serial.println("ERROR: Could not find RTC");
+    while(1);
+  }
+  if (rtc.lostPower()) {  //If RTC Lost power, reset time from PC
+    Serial.println("WARNING: RTC lost power, so we are assuming the current time is valid");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  // Set sensor structure
   sensor_t sensor;
 
- // Set delay between sensor readings based on sensor details.
-  delayMS = sensor.min_delay / 10000;
+ // Set delay between sensor readings
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
-  // Delay between measurements.
+  // Delay
   delay(delayMS);
-  // Get temperature event and print its value.
+
+  // RTC ----------------------------------------------------------------------------------------------------------//
+  // Read Date and Time - adjust over time
+  DateTime now = rtc.now();
+  sprintf(t, "%02d:%02d:%02d %02d/%02d/%02d",  now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year()); //adjust time over time 
+  Serial.print(F("Date/Time: ")); // Print Time to Serial Monitor
+  Serial.println(t);
+
+  // DHT11 ----------------------------------------------------------------------------------------------------------//
+  // Get temperature event and print its value
   sensors_event_t event;
   dht.temperature().getEvent(&event);
   if (isnan(event.temperature)) {
@@ -48,4 +85,5 @@ void loop() {
     Serial.print(event.relative_humidity);
     Serial.println(F("%"));
   }
+  //----------------------------------------------------------------------------------------------------------//
 }

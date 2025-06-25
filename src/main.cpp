f15d336b3eb290e2734082d2ff98ecd9c0b56f93
0022081 <benjamin10beard@gmail.com>
2025-06-26 10:15:24 +1200

    Updated platformio.ini to use ress997/TinyGPSPlusPlus@^0.0.4 and modified src/main.cpp to align with TinyGPSPlusPlus Library and added GPS stream for testing purposes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 
 // Including Libraries 
 #include <Arduino.h>
-//#include <Adafruit_Sensor.h>
-//#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 
 
-//#include <DHT.h>  // DHT11 Module
-//#include <DHT_U.h>      
+#include <DHT.h>  // DHT11 Module
+#include <DHT_U.h>      
 #include <TinyGPSPlus.h>  // GPS Module
 #include <SoftwareSerial.h> // Virtual Serial for GPS
 
 
 // Defining PINS------------------------------------------------------------------------//
-//#define DHTPIN 12
+#define DHTPIN 12
 
 // Grove GPS air 350 Module
-static const int RXPin = 4, TXPin = 8;
+static const int RXPin = 2, TXPin = 3;
 static const uint32_t GPSBaud = 9600;
 const long interval = 10000;
 unsigned long previousMillis = 0;
@@ -25,102 +25,47 @@ unsigned long previousMillis = 0;
 #define DHTTYPE    DHT11 
 
 //Define Objects ------------------------------------------------------------------------//
-//DHT_Unified dht(DHTPIN, DHTTYPE); //Create dht object
+DHT_Unified dht(DHTPIN, DHTTYPE); //Create dht object
 TinyGPSPlus gps; // The TinyGPSPlus object
 
 // The serial connection to the GPS device
 SoftwareSerial GPSserial(RXPin, TXPin);
 
-
-void setup() {
-  // Initialize devices
-  Serial.begin(9600); // Physical Serial
-  GPSserial.begin(GPSBaud); // Virtual Serial
-  //Wire.begin();
-  //dht.begin();
-
-  // Set DHT11 sensor -----------------------------------------------------------------------------------------------------//
-  //sensor_t sensor;
-
-  // Set delay between sensor readings
-}
-
-void loop() {
-  // DHT11 ----------------------------------------------------------------------------------------------------------//
-  // // Get temperature event and print its value
-  // sensors_event_t event;
-
-  // dht.temperature().getEvent(&event);
-  // if (isnan(event.temperature)) {
-  //   Serial.println(F("Error reading temperature!"));
-  // }
-  // else {
-  //   Serial.print(F("Temperature: "));
-  //   Serial.print(event.temperature);
-  //   Serial.println(F("°C"));
-  // }
-  // // Get humidity event and print its value.
-  // dht.humidity().getEvent(&event);
-  // if (isnan(event.relative_humidity)) {
-  //   Serial.println(F("Error reading humidity!"));
-  // }
-  // else {
-  //   Serial.print(F("Humidity: "));
-  //   Serial.print(event.relative_humidity);
-  //   Serial.println(F("%"));
-  // }
-  // GPS ----------------------------------------------------------------------------------------------------------//
-  // Feed the GPS parser as often as possible
-  while (GPSserial.available()) {
-    gps.encode(GPSserial.read());
-  }
-
-  // Print GPS info at fixed intervals 10sec
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    if (gps.location.isValid()) {
-      String GPSData = String(gps.location.lat(), 6) + "," + String(gps.location.lng(), 6);
-      Serial.println(GPSData);
-      Serial.println("GPS Data Printed");
-    } 
-    else {
-      Serial.println("Waiting for valid GPS data...");
-    }
-  }
-}
+// A sample NMEA stream.
+const char *gpsStream =
+"$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+"$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+"$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+"$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+"$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+"$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
 
 // Custom Functions -----------------------------------------------------------------------------------------//
-void displayInfo() {
+static void displayInfo() {
   Serial.print(F("Location: ")); 
-  if (gps.location.isValid())
-  {
+  if (gps.location.isValid()) {
     Serial.print(gps.location.lat(), 6);
     Serial.print(F(","));
     Serial.print(gps.location.lng(), 6);
   }
-  else
-  {
+  else  {
     Serial.print(F("INVALID"));
   }
 
   Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
+  if (gps.date.isValid()) {
     Serial.print(gps.date.month());
     Serial.print(F("/"));
     Serial.print(gps.date.day());
     Serial.print(F("/"));
     Serial.print(gps.date.year());
   }
-  else
-  {
+  else  {
     Serial.print(F("INVALID"));
   }
 
   Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
+  if (gps.time.isValid()) {
     if (gps.time.hour() < 10) Serial.print(F("0"));
     Serial.print(gps.time.hour());
     Serial.print(F(":"));
@@ -132,9 +77,7 @@ void displayInfo() {
     Serial.print(F("."));
     if (gps.time.centisecond() < 10) Serial.print(F("0"));
     Serial.print(gps.time.centisecond());
-  }
-  else
-  {
+  } else  {
     Serial.print(F("INVALID"));
   }
 
@@ -142,12 +85,66 @@ void displayInfo() {
 }
 
 // This custom version of delay() ensures that the gps object is being "fed"
-void smartDelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do {
-    while (GPSserial.available()) {
-      gps.encode(GPSserial.read());
-    }
-  } while (millis() - start < ms);
+static void smartDelay(unsigned long ms) {
+	unsigned long start = millis();
+	do {
+		while (GPSserial.available()) {
+			gps.encode(GPSserial.read());
+		}
+	} while (millis() - start < ms);
+}
+
+void setup() {
+  // Initialize devices
+  Serial.begin(9600); // Physical Serial
+  GPSserial.begin(GPSBaud); // Virtual Serial
+  Wire.begin();
+  dht.begin();
+
+  // Set DHT11 sensor -----------------------------------------------------------------------------------------------------//
+  sensor_t sensor;
+
+}
+
+void loop() {
+  // DHT11 ----------------------------------------------------------------------------------------------------------//
+  // Get temperature event and print its value
+  sensors_event_t event;
+
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+  // GPS ----------------------------------------------------------------------------------------------------------//
+  // Print No. Satellites fixed
+  Serial.print("Satellites: ");
+  if (gps.satellites.isValid()) {
+   Serial.println(gps.satellites.value());
+  } else {
+    Serial.println("INVALID");
+  }
+
+  displayInfo();
+
+  smartDelay(10000);
+
+  // If No data is encoded in 5s - Error
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+		Serial.println(F("No GPS data received: check wiring"));
+	}
 }

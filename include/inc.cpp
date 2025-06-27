84259821c60187f7b359a15f168a80c65205da8e
0022081 <benjamin10beard@gmail.com>
2025-06-27 13:11:38 +1200

    Added LoRa library and moved libraries to seperate file for organisation

---
+++ include/inc.cpp
@@ -0,0 +1,10 @@
+// Including Libraries 
+#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+
+#include <DHT.h>  // DHT11 Module
+#include <DHT_U.h>      
+#include <TinyGPSPlus.h>  // GPS Module
+#include <SoftwareSerial.h> // Virtual Serial for GPS
